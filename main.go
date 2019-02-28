61b71825baf2ddf46aa9a3049ab2e55be7006783
Igor Kim @ HfTL <gs2016007@hft-leipzig.de>
2019-02-28 12:16:49 +0100

    add API as a parameter

--- main.go
+++ main.go
@@ -15,14 +15,16 @@ var serv *server
 var max = flag.Int("hist", 10, "number of historical events to generate")
 var p = flag.Duration("period", 1*time.Minute, "how frequently to generate new alerts")
 var port = flag.Int("port", 8000, "http port")
+var apiServer = flag.String("api", "http://127.0.0.1:8081/", "http port")
+var apiToken = flag.String("token", "debug_token", "http port")
 
 func main() {
 	flag.Parse()
 	serv = newServer()
 
 	serv.apiConn = new(hftlapiconnector.APIClient)
-	serv.apiConn.APIKey = "debug_token"
-	serv.apiConn.BaseURL = "http://127.0.0.1:8081/"
+	serv.apiConn.APIKey = *apiToken
+	serv.apiConn.BaseURL = *apiServer
 	serv.apiConn.UserID = "kim"
 	serv.apiConn.InitREST()
 
